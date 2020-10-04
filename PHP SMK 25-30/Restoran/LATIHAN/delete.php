48d75b95a681b86ab117f2ebadf2d21010de0fe3
RizkyFahrizal <m.rizkyfahrizal1410@gmail.com>
2020-10-04 17:05:15 +0700

    pernstrukturan ulang folder

--- PHP SMK 25-30/Restoran/LATIHAN/delete.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    require_once"../function.php";
-    $sql="DELETE FROM tblkategori WHERE idkategori=$id";
-    $result=mysqli_query($koneksi,$sql);
-     header("location:http://localhost/PHPsmk/Restoran/Kategori/select.php");
-?>
--- PHP SMK 25-30/Restoran/orderdetail/select.php
+++
@@ -1,1 +0,0 @@
-<h1>Select order detail</h1>
