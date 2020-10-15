ce6b578dafcb0c6546a763e16e692d4ea8eec679
RizkyFahrizal <m.rizkyfahrizal1410@gmail.com>
2020-10-16 00:38:04 +0700

    PHP SMK 41-45
    minggu #14

--- Restoran/index.php
+++ Restoran/index.php
@@ -4,6 +4,11 @@
     $db=new DB;
     $sql="SELECT * FROM tblkategori ORDER BY kategori";
     $row=$db->getALL($sql);
+
+    if (isset($_GET['log'])) {
+        session_destroy();
+        header("location:index.php");
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,10 +25,23 @@
                 <h2><a href="index.php"> Restoran pak ijul</a></h2>
             </div>
             <div class="col-md-9">
-                <div class="float-right mt-4">Logout</div> 
-                <div class="float-right mt-4 mr-4">Login</div> 
-                <div class="float-right mt-4 mr-4">Pelanggan</div> 
-                <div class="float-right mt-4 mr-4">Daftar</div> 
+                <?php 
+                    if (isset($_SESSION['pelanggan'])) { 
+                        echo '
+                        <div class="float-right mt-4"><a href="?log=logout">Logout</a></div> 
+                        <div class="float-right mt-4 mr-4">Pelanggan : <a href="?f=home&m=beli">'.$_SESSION['pelanggan'].'</a></div> 
+                        ';
+                    }else { 
+                        echo' 
+                        <div class="float-right mt-4 mr-4"><a href="?f=home&m=login">Login</a></div> 
+                        <div class="float-right mt-4 mr-4"><a href="?f=home&m=daftar">Daftar</a></div> 
+                    ';
+                    }
+
+                ?>
+
+               
+                
             </div>
         </div>
         <div class="row mt-5">
